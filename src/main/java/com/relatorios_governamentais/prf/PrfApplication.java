f908812f8bc865fe7a8b8677848168f448dff5e4
william.cavalcante <william.cavalcante@ifood.com.br>
2024-06-26 08:57:22 -0300

    Initial commit

---
+++ src/main/java/com/relatorios_governamentais/prf/PrfApplication.java
@@ -0,0 +1,13 @@
+package com.relatorios_governamentais.prf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/relatorios_governamentais/prf/PrfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relatorios_governamentais.prf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
